2b276025c29ca760ab867a0d536718a6bbf1a4f7
BuildTools <lucas.naveteur@etu.u-pec.fr>
2019-09-19 19:05:08 +0200

    Rajout des règles de programmations, modifications et ajout de certains programmes

--- Lorem.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(void){
-    printf("Lorem ipsum dolor sit amet , consectetur adipiscing elit , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua .\nUt enim ad minim veniam , quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur .\n \nExcepteur sint occaecat cupidatat non proident , sunt in culpa qui officia deserunt mollit anim id est laborum .\n");
-    return 0;
-}
--- TP1/special.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(void){
-    printf("\\ \t \"backslash\"\n");
-    printf("\\\\ \t \"real backslash\"\n");
-    printf("\\\\\\ \t \"real real backslash\"\n");
-    printf("\\\\\\\\\\\\\\\\... \t \"the true name of Ba'al, the soul eater\"\n");
-    return 0;
-}

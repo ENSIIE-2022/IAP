756045d3112912e9b08bf083b3fef190fd982dd8
Tchebychev <lucas.naveteur@hotmail.fr>
2019-11-21 16:36:23 +0100

    Réparation de la fonction pgcdSimple

--- TP4/main.c
+++ TP4/main.c
@@ -11,6 +11,6 @@ int main(int argc, char** argv){
     printf("Rentrez les 2 nombre entiers : ");
     scanf("%d %d", &n, &m);
     printf("PGCD-nor = %d\n", pgcdSimple(n, m));
-    printf("PGCD-rec = %d\n", pgcdRec(n, m));
+    printf("PGCD-rec = %d\n", pgcdRec(n, m));   
     return 0;
 }
